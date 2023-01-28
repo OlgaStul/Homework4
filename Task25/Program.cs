55f99793473b11eefee28a9716c4a79e56aa782d
Olga <chi89@yandex.ru>
2023-01-28 23:43:51 +0300

    возводит число A в натуральную степень B

---
+++ Task25/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16   */
+
+int GetNumberA(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод");
+        }
+    }
+    return (result);
+}
+
+int GetNumberB(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод");
+        }
+    }
+    return (result);
+}
+
+int A = GetNumberA("Введите любое число A = ");
+int B = GetNumberB("Введите любое натуральное число B = ");
+
+double numberInPower = Math.Pow(A, B);
+Console.WriteLine($"Число {A} в степени {B} равно {numberInPower}");
+
+
+/* Функция возведения в степень
+void ToDegree(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine(result);
+}
+*/
